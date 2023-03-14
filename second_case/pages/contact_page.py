5b19a8226c7ad316c731ff36accb8ff17ef50a4e
maurolaurenzi <maurodlaurenzi@gmail.com>
2023-03-14 15:50:26 -0300

    refactoring contact_page field filling methods

--- second_case/pages/contact_page.py
+++ second_case/pages/contact_page.py
@@ -30,45 +30,33 @@ class ContactPage:
         door_count = self.driver.find_element(*EMAIL_INPUT)
         return door_count.is_displayed()
     
+    def clear_field(self, input_field):
+        field = self.driver.find_element(*input_field)
+        field.click()
+        field.send_keys(Keys.END)
+        for i in range(len(field.get_attribute("value"))):
+            field.send_keys(Keys.BACKSPACE)
+        return field
+    
     #For the "fill" methods, we first clear the field and then send the input
     def fill_first_name_field(self, input):
-        first_name = self.driver.find_element(*FIRST_NAME_INPUT)
-        first_name.click()
-        first_name.send_keys(Keys.END)
-        for i in range(len(first_name.get_attribute("value"))):
-            first_name.send_keys(Keys.BACKSPACE)
+        first_name = self.clear_field(FIRST_NAME_INPUT)
         first_name.send_keys(input)
 
     def fill_last_name_field(self, input):
-        last_name = self.driver.find_element(*LAST_NAME_INPUT)
-        last_name.click()
-        last_name.send_keys(Keys.END)
-        for i in range(len(last_name.get_attribute("value"))):
-            last_name.send_keys(Keys.BACKSPACE)
+        last_name = self.clear_field(LAST_NAME_INPUT)
         last_name.send_keys(input)
     
     def fill_email_field(self, input):
-        email = self.driver.find_element(*EMAIL_INPUT)
-        email.click()
-        email.send_keys(Keys.END)
-        for i in range(len(email.get_attribute("value"))):
-            email.send_keys(Keys.BACKSPACE)
+        email = self.clear_field(EMAIL_INPUT)
         email.send_keys(input)
     
     def fill_phone_field(self, input):
-        phone = self.driver.find_element(*PHONE_INPUT)
-        phone.click()
-        phone.send_keys(Keys.END)
-        for i in range(len(phone.get_attribute("value"))):
-            phone.send_keys(Keys.BACKSPACE)
+        phone = self.clear_field(PHONE_INPUT)
         phone.send_keys(input)
     
     def fill_door_count_field(self, input):
-        door_count = self.driver.find_element(*DOOR_COUNT_INPUT)
-        door_count.click()
-        door_count.send_keys(Keys.END)
-        for i in range(len(door_count.get_attribute("value"))):
-            door_count.send_keys(Keys.BACKSPACE)
+        door_count = self.clear_field(DOOR_COUNT_INPUT)
         door_count.send_keys(input)
     
     def enter_name_error_is_displayed(self):
