3be38301073448a66c7d7d46c34add3531c07b2d
maurolaurenzi <maurodlaurenzi@gmail.com>
2023-12-17 00:10:39 -0300

    updating contact page objects

--- second_case/pages/contact_page.py
+++ second_case/pages/contact_page.py
@@ -6,28 +6,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class ContactPage(BasePage):
-    # Methods to check if the fields are displayed
-    def first_name_input_is_displayed(self):
-        first_name = self.driver.find_element(*FIRST_NAME_INPUT)
-        return first_name.is_displayed()
     
-    def last_name_input_is_displayed(self):
-        last_name = self.driver.find_element(*LAST_NAME_INPUT)
-        return last_name.is_displayed()
-    
-    def email_input_is_displayed(self):
-        email = self.driver.find_element(*EMAIL_INPUT)
-        return email.is_displayed()
-    
-    def phone_input_is_displayed(self):
-        phone = self.driver.find_element(*PHONE_INPUT)
-        return phone.is_displayed()
-    
-    def door_count_input_is_displayed(self):
-        door_count = self.driver.find_element(*EMAIL_INPUT)
-        return door_count.is_displayed()
-    
-    # Method to remove any charachter that the field might have, it returns the input field empty
+    # Method to remove any character that the field might have, it returns the input field empty
     def clear_field(self, input_field):
         field = self.driver.find_element(*input_field)
         field.click()
@@ -57,33 +37,22 @@ class ContactPage(BasePage):
         door_count = self.clear_field(DOOR_COUNT_INPUT)
         door_count.send_keys(input)
 
-    # Methods to check if the error messages are displayed
-    def enter_name_error_is_displayed(self):
-        self.wait_until_element_present(BLANK_NAME_ERROR)
-        return self.driver.find_element(*BLANK_NAME_ERROR).is_displayed()
+    def select_option(self, input):
+        dropdownElement = dropdown_option(input)
+        option = self.driver.find_element(*dropdownElement)
+        option.click()
+
+    def select_asset_class(self, input):
+        asset_btn = self.driver.find_element(*ASSET_CLASS_DROPDOWN)
+        asset_btn.click()
+        self.select_option(input)
     
-    def enter_phone_error_is_displayed(self):
-        self.wait_until_element_present(BLANK_PHONE_ERROR)
-        return self.driver.find_element(*BLANK_PHONE_ERROR).is_displayed()
+    def select_software_used(self, input):
+        software_btn = self.driver.find_element(*SOFTWARE_DROPDOWN)
+        software_btn.click()
+        self.select_option(input)
     
-    def blank_door_count_error_is_displayed(self):
-        self.wait_until_element_present(BLANK_DOOR_COUNT_ERROR)
-        return self.driver.find_element(*BLANK_DOOR_COUNT_ERROR).is_displayed()
-    
-    def invalid_phone_error_is_displayed(self):
-        self.wait_until_element_present(INVALID_PHONE_ERROR)
-        return self.driver.find_element(*INVALID_PHONE_ERROR).is_displayed()
-    
-    # Method to check if the submit button is available
-    def submit_button_is_enabled(self):
-        return self.driver.find_element(*SUBMIT_BTN).is_enabled()
-    
-    def wait_until_submit_button_is_available(self, is_enabled=True):
-      if is_enabled:
-        WebDriverWait(self.driver, 10).until(lambda driver: self.submit_button_is_enabled())
-      else:
-        WebDriverWait(self.driver, 10).until_not(lambda driver: self.submit_button_is_enabled())
-    
-    # Method to wait until an element is present (not visible) in the DOM
-    def wait_until_element_present(self, element):
-        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(element))
+    # Clicking the submit form button
+    def submit_form(self):
+        submit_btn = self.driver.find_element(*SUBMIT_BTN)
+        submit_btn.click()
